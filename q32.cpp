3a0d637cc6e3120feb03f4ec6382a235e8cf68e6
theskinnyhippo <134091490+theskinnyhippo@users.noreply.github.com>
2023-10-11 08:16:53 +0530

    Update q32.cpp

--- q32.cpp
+++ q32.cpp
@@ -1,21 +1,89 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class Quadriatic{
-    private :
-        double a,b,c;
 
-    public :
-        Quadriatic() : a(0), b(0), c(0) {}
-        Quadriatic(const double p,const double q, const double r) : a(p), b(q), c(r) {}
-        Quadriatic(const Quadriatic &other) {
-            Quadriatic(other.a, other.b, other.c);
-        }
-       ~Quadriatic(){}
+class Complex {
+public:
+    double real;
+    double imag;
 
-        Quadriatic operator + (const Quadriatic &other) const {
-            return Quadriatic(a+other.a, b+other.b, c+other.c);
-        }
+    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
 
-        Quadriatic operator >> 
+    friend ostream& operator <<(ostream& os, const Complex& c) {
+        if (c.imag >= 0)
+            os << c.real << " + " << c.imag << "i";
+        else
+            os << c.real << " - " << -c.imag << "i";
+        return os;
+    }
 };
+
+class Quadratic {
+private:
+    double a, b, c;
+
+public:
+    Quadratic(double a = 0, double b = 0, double c = 0) : a(a), b(b), c(c) {}
+
+    friend Quadratic operator+(const Quadratic& q1, const Quadratic& q2) {
+        return Quadratic(q1.a + q2.a, q1.b + q2.b, q1.c + q2.c);
+    }
+
+    friend ostream& operator<<(ostream& os, const Quadratic& q) {
+        os << q.a << "x^2";
+        if (q.b >= 0)
+            os << " + " << q.b << "x";
+        else
+            os << " - " << -q.b << "x";
+        if (q.c >= 0)
+            os << " + " << q.c;
+        else
+            os << " - " << -q.c;
+        return os;
+    }
+
+    friend istream& operator>>(istream& is, Quadratic& q) {
+        is >> q.a >> q.b >> q.c;
+        return is;
+    }
+
+    double evaluate(double x) {
+        return a * x * x + b * x + c;
+    }
+
+    pair<Complex, Complex> findRoots() {
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant >= 0) {
+            double sqrtDiscriminant = sqrt(discriminant);
+            double root1 = (-b + sqrtDiscriminant) / (2 * a);
+            double root2 = (-b - sqrtDiscriminant) / (2 * a);
+            return {Complex(root1, 0), Complex(root2, 0)};
+        } else {
+            double realPart = -b / (2 * a);
+            double imaginaryPart = sqrt(-discriminant) / (2 * a);
+            return {Complex(realPart, imaginaryPart), Complex(realPart, -imaginaryPart)};
+        }
+    }
+};
+
+int main() {
+    Quadratic q1, q2;
+    cout << "Enter the coefficients of the first quadratic equation (a, b, c): ";
+    cin >> q1;
+    cout << "Enter the coefficients of the second quadratic equation (a, b, c): ";
+    cin >> q2;
+
+    Quadratic sum = q1 + q2;
+    cout << "Sum of the two quadratic equations: " << sum << endl;
+
+    double x;
+    cout << "Enter a value for x to evaluate the first quadratic equation: ";
+    cin >> x;
+    cout << "Result of the evaluation: " << q1.evaluate(x) << endl;
+
+    pair<Complex, Complex> roots = q1.findRoots();
+    cout << "Roots of the first quadratic equation: " << roots.first << " and " << roots.second << endl;
+
+    return 0;
+}
+
